68888189ebd0d6398b48601292e66bf165868cc9
Phil Smithies <philsmithies@gmail.com>
2021-11-26 00:37:09 +0100

    profile can filter images

--- src/Pages/Profile/index.jsx
+++ src/Pages/Profile/index.jsx
@@ -2,11 +2,8 @@ import "./index.css";
 import React, { useState, useEffect } from "react";
 import Axios from "axios";
 import { Image } from "cloudinary-react";
-import { UserContext } from "../../Contexts/UserContext";
-import { useContext } from "react";
-import { useParams } from "react-router-dom";
+import { useParams, Link } from "react-router-dom";
 import Typography from "@material-ui/core/Typography";
-import { Link } from "react-router-dom";
 
 function Profile() {
   const [data, setData] = useState();
@@ -37,14 +34,12 @@ function Profile() {
       }).then((res) => {
         console.log(res.data);
         setData(res.data);
-        // console.log(res.data)
-        // return (res.data);
       });
     }
   };
 
   const filterPictures = function (pics) {
-    // console.log(pics);
+    console.log(pics);
     const filteredPics = pics.filter(
       (picture) => data && picture.author === data.username
     );
@@ -63,7 +58,7 @@ function Profile() {
     // console.log(userData);
     // setData(userData);
     // });
-    Axios("http://localhost:3001/photos").then(function (response) {
+    Axios("http://localhost:3001/photos/all").then(function (response) {
       console.log(response.data);
       setAllPictures(response.data);
       // setFilteredData(response.data);
@@ -75,26 +70,6 @@ function Profile() {
   useEffect(() => {
     filterPictures(allPictures);
   }, [data, allPictures]);
-
-  //--------------------------------------------------------
-
-  // useEffect(() => {
-  //   Axios("http://localhost:3001/photos")
-  //   .then((response) => {
-  //     console.log(response.data);
-  //     setAllData(response.data);
-  //     setFilteredData(response.data);
-
-  //     const filteredPics = filteredData.filter(picture => data && picture.author === data.username)
-  //     console.log
-  //     setPics(filteredPics);
-  //   })
-  //   .catch((error) => {
-  //     console.log("Error getting data: " + error);
-  //   });
-  // }, []);
-
-  //----------------------------------------------------------
 
   return (
     <div>
